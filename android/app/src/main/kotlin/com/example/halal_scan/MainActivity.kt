3c687b74efc45cbe0c7730090be0391ad5e731fd
Isko21 <iskosstyle@gmail.com>
2023-01-07 21:45:39 -0100

    app check added

--- android/app/src/main/kotlin/com/example/halal_scan/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/halal_scan/MainActivity.kt
@@ -1,6 +1,18 @@
 package io.isko.halalscan
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory  
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(/*context=*/this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            SafetyNetAppCheckProviderFactory.getInstance()
+        )
+        super.onCreate(savedInstanceState)
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -17,6 +18,8 @@ import 'utility/auth.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate();
+
   runApp(const MyApp());
 }
 
